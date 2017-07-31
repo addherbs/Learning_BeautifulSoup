cf821b8fca75cdadefd9b329366d824cbc948c93
Aditya Pandey <addherbs@gmail.com>
2017-07-30 23:26:34 -0500

    updated google search

--- search_google.py
+++ search_google.py
@@ -3,6 +3,9 @@ from google import search
 
 song_name = 'main+tera+hero'
 prefix = 'https://www.google.com/search?q=lyricsmint.com+main+tera+hero'
+
+#Not original results
+#About 40 results per hour
 ## gives google results
 def search_google():
     count = 0

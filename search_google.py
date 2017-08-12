210ad2ae95cb9a3d118efdd87273e6b1d0d4cf89
Aditya Pandey <addherbs@gmail.com>
2017-08-11 23:37:50 -0500

    changes search google results

--- search_google.py
+++ search_google.py
@@ -11,7 +11,7 @@ def search_google():
     count = 0
     links = []
     for link in search('www.google.com'):
-        links.append(link)
         count = count +1
+		links.append(link)
         if count == 10:
             break
